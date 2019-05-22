02eef8507d57bcbe4e22c51d61bc094f18f9ff42
Stuart Pearce <spearce@coas.oregonstate.edu>
2019-05-22 13:53:29 -0700

    a temporary branch that corrects chlorophyll for 383-8

--- ooidba_to_ngdac_profile_nc.py
+++ ooidba_to_ngdac_profile_nc.py
@@ -199,6 +199,11 @@ def main(args):
             if dba is None:
                 continue
 
+        # Re_calculate chlorophyll for 383-00008
+        dba = processing.recalc_chlor(dba, dark_offset=50, scale_factor=0.1004)
+        if dba is None:
+            continue
+
         if 'sci_flbbcd_bb_units' in dba.sensor_names:
             dba = processing.backscatter_total(dba)
             if dba is None:
